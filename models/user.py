20615eb5668f85104c9f972503d49a395ee2d2ed
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-02 16:01:45 +0200

    Relationship with user and review

--- models/user.py
+++ models/user.py
@@ -4,14 +4,16 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class User(BaseModel):
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
 
-places = relationship("Place", cascade='all, delete', backref='cities')
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
