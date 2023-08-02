2e671f51fba33ad4c5603100e66136fd3f25ac2f
Air-KS <kevinrogeret@gmail.com>
2023-08-02 15:32:16 +0200

    Correct position of ForeignKey

--- models/place.py
+++ models/place.py
@@ -7,8 +7,8 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = 'Places'
-    city_id = Column(String(60), nullable=False, ForeignKey'cities.id')
-    user_id = Column(String(60), nullable=False, ForeignKey'cities.id')
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
     number_rooms = Column(Integer, nullable=False, default=0)
