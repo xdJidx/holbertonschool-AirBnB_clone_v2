3362fe3b91a7d809b9c432865709803795b151e5
Air-KS <kevinrogeret@gmail.com>
2023-08-02 15:33:18 +0200

    Correct Pycodestyle

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = 'Places'
