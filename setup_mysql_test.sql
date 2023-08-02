213415c8db8c080b976522be0dbe2bef0ae67796
xdJidx <bernardjerome59@gmail.com>
2023-08-02 10:30:40 +0200

    Remove line USE

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,7 @@
 -- script that prepares a MySQL server
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
-USE hbnb_dev_db;
-        
+       
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
     
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
