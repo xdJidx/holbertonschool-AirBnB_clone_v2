b327d17b715aa2a68373b0d9773fd4f3570979e9
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-02 14:58:43 +0200

    Conditions for import dbstorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+import os
 
+    # Vérifier la valeur de la variable d'env
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        #alors importer la classe DBstorage
+        from db_storage import DBStorage
+        # Créer une instance de DBStorage et la stocker dans storage
+        storage = DBStorage()
+    else:
+        from file_storage import FileStorage
+        storage = FileStorage()
 
-storage = FileStorage()
-storage.reload()
+    storage.reload()
