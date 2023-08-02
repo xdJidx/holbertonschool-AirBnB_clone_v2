a5f603c92d4e05560eface147e9a1fffacec8e05
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-02 14:24:51 +0200

    Update File and relationship

--- models/place.py
+++ models/place.py
@@ -5,14 +5,15 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'Places'
+    city_id = Column(String(60), nullable=False, ForeignKey'cities.id')
+    user_id = Column(String(60), nullable=False, ForeignKey'cities.id')
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
