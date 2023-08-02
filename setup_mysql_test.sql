26f40aced8dbd3351035000aef20412d332f5293
xdJidx <bernardjerome59@gmail.com>
2023-08-02 10:29:34 +0200

    Setup MySQL test server

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- script that prepares a MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+USE hbnb_dev_db;
+        
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+    
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON  performance_schema.* TO 'hbnb_test'@'localhost';
