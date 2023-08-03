075f1b7659a30af9da1327bda5f79e927596bbb2
Air-KS <kevinrogeret@gmail.com>
2023-08-03 14:28:21 +0000

    Refonte base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from datetime import datetime
 from models import storage
 from sqlalchemy import Column, PrimaryKeyConstraint, String, DateTime
+from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -13,9 +13,9 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(String(60), nullable=False, PrimaryKey=True)
-    created_at = Column(datetime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(datetime, nullable=False, default=datetime.utcnow())
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
