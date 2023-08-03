c4712b26da150eb1918c1d8c34999993458197d1
xdJidx <bernardjerome59@gmail.com>
2023-08-03 14:12:10 +0200

    Update comment

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,11 @@
-﻿-- Comment
-
+﻿-- script that prepares a MySQL server
+-- because we need to setup the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 USE hbnb_dev_db;
-        
+
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-    
+
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 GRANT SELECT ON  performance_schema.* TO 'hbnb_dev'@'localhost';
