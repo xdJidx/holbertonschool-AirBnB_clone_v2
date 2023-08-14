6a4e106dcb10fae1f01bdd016f48d3ef547e1a89
xdJidx <bernardjerome59@gmail.com>
2023-08-14 11:28:28 +0200

    Task 4 : Is it a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Add replace underscores with spaces.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number_route(n):
+    if n.isdigit():
+        return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
