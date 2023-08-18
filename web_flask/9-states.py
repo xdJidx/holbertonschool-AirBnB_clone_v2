a4e60f3f6580b07f94220a419ec5feacbbef38f2
xdJidx <bernardjerome59@gmail.com>
2023-08-18 10:22:53 +0200

    Correction web flask

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,11 +13,13 @@ app = Flask(__name__)
 def close_db(exception):
     storage.close()
 
+
 @app.route('/states', strict_slashes=False)
 def list_states():
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('states.html', states=sorted_states)
+
 
 @app.route('/states/<id>', strict_slashes=False)
 def list_cities_by_state(id):
@@ -27,6 +29,7 @@ def list_cities_by_state(id):
         return render_template('cities.html', state=state, cities=cities)
     else:
         return render_template('not_found.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
